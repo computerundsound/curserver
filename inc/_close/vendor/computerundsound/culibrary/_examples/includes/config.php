b933da41c7f3af198c0a9fadb501a5ff404ed590
cu <cugit@cusp.de>
2019-05-10 00:35:07 +0200

    Try to add vendor-dir to main git

--- inc/_close/vendor/computerundsound/culibrary/_examples/includes/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-/* For testing => you can insert your own credentials here */
-define('DB_USERNAME', 'culibrary');
-define('DB_PASSWORD', 'culibrary');
-define('DB_SERVER', 'localhost');
-define('DB_DB_NAME', 'culibrary');
